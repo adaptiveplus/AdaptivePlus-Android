79ffc192dba34a8b3f87e17cab80f803fc356b29
Alexey Muryshkin <alexey@Alexeys-MacBook-Pro.local>
2021-04-14 02:19:39 +0600

    feat(#7m0ktq): track image & gif loading progress

---
+++ adaptiveplus/src/main/java/com/sprintsquads/adaptiveplus/core/glide/GlideWrappingTarget.kt
@@ -0,0 +1,57 @@
+package com.sprintsquads.adaptiveplus.core.glide
+
+import android.graphics.drawable.Drawable
+import com.bumptech.glide.request.Request
+import com.bumptech.glide.request.target.SizeReadyCallback
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.transition.Transition
+
+
+internal open class GlideWrappingTarget<Z>(
+    private val target: Target<Z>
+) : Target<Z> {
+
+    override fun onStart() {
+        target.onStart()
+    }
+
+    override fun onStop() {
+        target.onStop()
+    }
+
+    override fun onDestroy() {
+        target.onDestroy()
+    }
+
+    override fun onLoadStarted(placeholder: Drawable?) {
+        target.onLoadStarted(placeholder)
+    }
+
+    override fun onLoadFailed(errorDrawable: Drawable?) {
+        target.onLoadFailed(errorDrawable)
+    }
+
+    override fun onResourceReady(resource: Z, transition: Transition<in Z>?) {
+        resource?.let { target.onResourceReady(resource, transition) }
+    }
+
+    override fun onLoadCleared(placeholder: Drawable?) {
+        target.onLoadCleared(placeholder)
+    }
+
+    override fun getSize(cb: SizeReadyCallback) {
+        target.getSize(cb)
+    }
+
+    override fun removeCallback(cb: SizeReadyCallback) {
+        target.removeCallback(cb)
+    }
+
+    override fun setRequest(request: Request?) {
+        target.request = request
+    }
+
+    override fun getRequest(): Request? {
+        return target.request
+    }
+}
--- adaptiveplus/src/main/java/com/sprintsquads/adaptiveplus/extensions/ImageViewExtension.kt
+++ adaptiveplus/src/main/java/com/sprintsquads/adaptiveplus/extensions/ImageViewExtension.kt
@@ -1,5 +1,6 @@
 package com.sprintsquads.adaptiveplus.extensions
 
+import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
@@ -9,7 +10,9 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.gif.GifDrawable
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.ImageViewTarget
 import com.bumptech.glide.request.target.Target
+import com.sprintsquads.adaptiveplus.core.glide.GlideProgressTarget
 import com.sprintsquads.adaptiveplus.data.GLIDE_TIMEOUT
 
 /**
@@ -17,8 +20,9 @@ import com.sprintsquads.adaptiveplus.data.GLIDE_TIMEOUT
  *
  * @param url - image url
  * @param defaultDrawable - default drawable (placeholder)
- * @param onResourceReady - on resource loaded callback
  * @param cornerRadius - corner radius to round the image
+ * @param onResourceReady - on resource loaded callback
+ * @param onLoadFailed - on resource load failed callback
  */
 internal fun ImageView.loadImage(
     url: String,
@@ -34,16 +38,30 @@ internal fun ImageView.loadImage(
             RequestOptions()
         }
 
+    val imageViewTarget = object: ImageViewTarget<Bitmap>(this) {
+        override fun setResource(resource: Bitmap?) {
+            setImageBitmap(resource)
+        }
+    }
+    val target = object: GlideProgressTarget<String, Bitmap>(url, imageViewTarget) {
+        override fun onDownloading(bytesRead: Long, expectedLength: Long) {
+            super.onDownloading(bytesRead, expectedLength)
+            val progress = bytesRead * 100f / expectedLength
+            // TODO: handle progress
+        }
+    }
+
     Glide
         .with(context)
+        .asBitmap()
         .load(url)
         .timeout(GLIDE_TIMEOUT)
         .placeholder(defaultDrawable)
-        .listener(object: RequestListener<Drawable> {
+        .listener(object: RequestListener<Bitmap> {
             override fun onLoadFailed(
                 e: GlideException?,
                 model: Any?,
-                target: Target<Drawable>?,
+                target: Target<Bitmap>?,
                 isFirstResource: Boolean
             ): Boolean {
                 onLoadFailed?.invoke()
@@ -51,9 +69,9 @@ internal fun ImageView.loadImage(
             }
 
             override fun onResourceReady(
-                resource: Drawable?,
+                resource: Bitmap?,
                 model: Any?,
-                target: Target<Drawable>?,
+                target: Target<Bitmap>?,
                 dataSource: DataSource?,
                 isFirstResource: Boolean
             ): Boolean {
@@ -62,16 +80,17 @@ internal fun ImageView.loadImage(
             }
         })
         .apply(requestOptions)
-        .into(this)
+        .into(target)
 }
 
 /**
- * Extension function to load and display image in ImageView
+ * Extension function to load and display GIF in ImageView
  *
  * @param url - image url
  * @param defaultDrawable - default drawable (placeholder)
- * @param onResourceReady - on resource loaded callback
  * @param cornerRadius - corner radius to round the image
+ * @param onResourceReady - on resource loaded callback
+ * @param onLoadFailed - on resource load failed callback
  */
 internal fun ImageView.loadGIF(
     url: String,
@@ -86,6 +105,19 @@ internal fun ImageView.loadGIF(
         } else {
             RequestOptions()
         }
+
+    val imageViewTarget = object: ImageViewTarget<GifDrawable>(this) {
+        override fun setResource(resource: GifDrawable?) {
+            setImageDrawable(resource)
+        }
+    }
+    val target = object: GlideProgressTarget<String, GifDrawable>(url, imageViewTarget) {
+        override fun onDownloading(bytesRead: Long, expectedLength: Long) {
+            super.onDownloading(bytesRead, expectedLength)
+            val progress = bytesRead * 100f / expectedLength
+            // TODO: handle progress
+        }
+    }
 
     Glide
         .with(context)
@@ -116,5 +148,5 @@ internal fun ImageView.loadGIF(
             }
         })
         .apply(requestOptions)
-        .into(this)
+        .into(target)
 }
