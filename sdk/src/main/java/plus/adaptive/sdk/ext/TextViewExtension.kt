a9390962960916a157a20a54d6bdbbeb3effbe8d
Alexey Muryshkin <alexey@Alexeys-MacBook-Pro.local>
2021-05-05 03:27:08 +0600

    fix: set text autoscale if size does not fit & add argument responsible for text size dimension unit

--- sdk/src/main/java/plus/adaptive/sdk/ext/TextViewExtension.kt
+++ sdk/src/main/java/plus/adaptive/sdk/ext/TextViewExtension.kt
@@ -9,6 +9,7 @@ import android.view.Gravity
 import android.widget.TextView
 import androidx.core.provider.FontRequest
 import androidx.core.provider.FontsContractCompat
+import androidx.core.widget.TextViewCompat
 import plus.adaptive.sdk.R
 import plus.adaptive.sdk.data.models.APFont
 import plus.adaptive.sdk.utils.getColorFromHex
@@ -18,10 +19,14 @@ private val apFontMap = mutableMapOf<String, Typeface>()
 
 internal fun TextView.applyAPFont(
     apFont: APFont,
+    dimensionUnit: Int = TypedValue.COMPLEX_UNIT_PX,
     onSuccess: (() -> Unit)? = null,
     onError: (() -> Unit)? = null
 ) {
-    setTextSize(TypedValue.COMPLEX_UNIT_PX, apFont.size.toFloat())
+    TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration(
+        this,
+        1, apFont.size.toInt(),
+        1, dimensionUnit)
     getColorFromHex(apFont.color)?.let { setTextColor(it) }
 
     gravity = when (apFont.align) {
@@ -35,7 +40,10 @@ internal fun TextView.applyAPFont(
     }
 
     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
-        apFont.lineHeight?.let { lineHeight = it.toInt() }
+        apFont.lineHeight?.let {
+            lineHeight = TypedValue.applyDimension(
+                dimensionUnit, it.toFloat(), resources.displayMetrics).toInt()
+        }
     }
 
     val fontMapKey = "${apFont.family}:${apFont.style}"
