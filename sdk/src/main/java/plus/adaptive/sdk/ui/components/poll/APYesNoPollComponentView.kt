6b9f0a1921d6dc384e263c4555d6f0536703354e
Alexey Muryshkin <alexey@Alexeys-MacBook-Pro.local>
2021-06-16 01:36:12 +0600

    feat(#900whm): set all constraints programmatically for YesNoPoll

--- sdk/src/main/java/plus/adaptive/sdk/ui/components/poll/APYesNoPollComponentView.kt
+++ sdk/src/main/java/plus/adaptive/sdk/ui/components/poll/APYesNoPollComponentView.kt
@@ -3,6 +3,7 @@ package plus.adaptive.sdk.ui.components.poll
 import android.content.Context
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
+import android.graphics.drawable.GradientDrawable
 import android.os.Build
 import android.util.AttributeSet
 import android.util.TypedValue
@@ -17,6 +18,7 @@ import plus.adaptive.sdk.data.models.components.APPollComponent
 import plus.adaptive.sdk.ext.hide
 import plus.adaptive.sdk.ui.components.core.APBaseComponentView
 import plus.adaptive.sdk.ui.components.core.vm.APComponentViewModel
+import plus.adaptive.sdk.utils.createDrawableFromColor
 import plus.adaptive.sdk.utils.getColorFromHex
 
 
@@ -39,6 +41,86 @@ internal class APYesNoPollComponentView : APBaseComponentView {
 
     override fun prepare() {
         apPollCardView?.radius = 16f * BASE_SIZE_MULTIPLIER
+        apPollLinearLayout?.background = createDrawableFromColor(
+            color = getColorFromHex("#0000"),
+            cornerRadius = 16 * BASE_SIZE_MULTIPLIER,
+            strokeWidth = 1 * BASE_SIZE_MULTIPLIER,
+            strokeColor = getColorFromHex("#DFE2E7")
+        )
+        apQuestionTextView?.apply {
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, 18f * BASE_SIZE_MULTIPLIER)
+            (layoutParams as LayoutParams).apply {
+                val margin = 12 * BASE_SIZE_MULTIPLIER
+                setMargins(margin, margin, margin, 0)
+            }
+        }
+        apAnswersCardView?.apply {
+            radius = 8f * BASE_SIZE_MULTIPLIER
+            cardElevation = 8f * BASE_SIZE_MULTIPLIER
+            (layoutParams as LayoutParams).apply {
+                val marginTop = 16 * BASE_SIZE_MULTIPLIER
+                val margin = 12 * BASE_SIZE_MULTIPLIER
+                setMargins(margin, marginTop, margin, margin)
+            }
+        }
+        apAnswersConstraintLayout?.run {
+            background = createDrawableFromColor(
+                color = getColorFromHex("#0000"),
+                cornerRadius = 8 * BASE_SIZE_MULTIPLIER,
+                strokeWidth = 1 * BASE_SIZE_MULTIPLIER,
+                strokeColor = getColorFromHex("#DFE2E7")
+            )
+
+            val constraintSet = ConstraintSet()
+            constraintSet.clone(this)
+
+            constraintSet.constrainWidth(R.id.apAnswerDivider, 1 * BASE_SIZE_MULTIPLIER)
+            constraintSet.setMargin(R.id.apLeftAnswerTextView, ConstraintSet.START, 4 * BASE_SIZE_MULTIPLIER)
+            constraintSet.setMargin(R.id.apLeftAnswerTextView, ConstraintSet.END, 4 * BASE_SIZE_MULTIPLIER)
+            constraintSet.setMargin(R.id.apLeftAnswerRatioTextView, ConstraintSet.TOP, 2 * BASE_SIZE_MULTIPLIER)
+            constraintSet.setVisibility(R.id.apLeftAnswerRatioTextView, View.GONE)
+            constraintSet.setMargin(R.id.apRightAnswerTextView, ConstraintSet.START, 4 * BASE_SIZE_MULTIPLIER)
+            constraintSet.setMargin(R.id.apRightAnswerTextView, ConstraintSet.END, 4 * BASE_SIZE_MULTIPLIER)
+            constraintSet.setMargin(R.id.apRightAnswerRatioTextView, ConstraintSet.TOP, 2 * BASE_SIZE_MULTIPLIER)
+            constraintSet.setVisibility(R.id.apRightAnswerRatioTextView, View.GONE)
+            constraintSet.setGuidelineBegin(R.id.apTopGuideline, 4 * BASE_SIZE_MULTIPLIER)
+            constraintSet.setGuidelineEnd(R.id.apBottomGuideline, 4 * BASE_SIZE_MULTIPLIER)
+
+            constraintSet.applyTo(this)
+        }
+        apLeftAnswerLayer?.background = GradientDrawable().apply {
+            getColorFromHex("#FFFFFF")?.let { setColor(it) }
+            shape = GradientDrawable.RECTANGLE
+            val radius = 8f * BASE_SIZE_MULTIPLIER
+            cornerRadii = floatArrayOf(radius, radius, 0f, 0f, 0f, 0f, radius, radius)
+        }
+        apRightAnswerLayer?.background = GradientDrawable().apply {
+            getColorFromHex("#FFFFFF")?.let { setColor(it) }
+            shape = GradientDrawable.RECTANGLE
+            val radius = 8f * BASE_SIZE_MULTIPLIER
+            cornerRadii = floatArrayOf(0f, 0f, radius, radius, radius, radius, 0f, 0f)
+        }
+        apLeftAnswerTextView?.apply {
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, 16f * BASE_SIZE_MULTIPLIER)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                lineHeight = 20 * BASE_SIZE_MULTIPLIER
+            }
+        }
+        apLeftAnswerRatioTextView?.apply {
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, 20f * BASE_SIZE_MULTIPLIER)
+        }
+        apRightAnswerTextView?.apply {
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, 16f * BASE_SIZE_MULTIPLIER)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                lineHeight = 20 * BASE_SIZE_MULTIPLIER
+            }
+        }
+        apRightAnswerRatioTextView?.apply {
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, 20f * BASE_SIZE_MULTIPLIER)
+        }
+
         reset()
     }
 
@@ -61,10 +143,10 @@ internal class APYesNoPollComponentView : APBaseComponentView {
 
             apLeftAnswerTextView?.apply {
                 text = pollData.answers[0].text
-                setTextSize(TypedValue.COMPLEX_UNIT_PX, 16f)
+                setTextSize(TypedValue.COMPLEX_UNIT_PX, 16f * BASE_SIZE_MULTIPLIER)
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    lineHeight = 20
+                    lineHeight = 20 * BASE_SIZE_MULTIPLIER
                 }
             }
             apLeftAnswerRatioTextView?.hide()
@@ -76,10 +158,10 @@ internal class APYesNoPollComponentView : APBaseComponentView {
 
             apRightAnswerTextView?.apply {
                 text = pollData.answers[1].text
-                setTextSize(TypedValue.COMPLEX_UNIT_PX, 16f)
+                setTextSize(TypedValue.COMPLEX_UNIT_PX, 16f * BASE_SIZE_MULTIPLIER)
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    lineHeight = 20
+                    lineHeight = 20 * BASE_SIZE_MULTIPLIER
                 }
             }
             apRightAnswerRatioTextView?.hide()
@@ -129,12 +211,12 @@ internal class APYesNoPollComponentView : APBaseComponentView {
         apLeftAnswerRatioTextView?.text = "$leftAnswerRatio%"
         apRightAnswerRatioTextView?.text = "$rightAnswerRatio%"
 
-        apLeftAnswerTextView?.setTextSize(TypedValue.COMPLEX_UNIT_PX, 14f)
-        apRightAnswerTextView?.setTextSize(TypedValue.COMPLEX_UNIT_PX, 14f)
+        apLeftAnswerTextView?.setTextSize(TypedValue.COMPLEX_UNIT_PX, 14f * BASE_SIZE_MULTIPLIER)
+        apRightAnswerTextView?.setTextSize(TypedValue.COMPLEX_UNIT_PX, 14f * BASE_SIZE_MULTIPLIER)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            apLeftAnswerTextView?.lineHeight = 15
-            apRightAnswerTextView?.lineHeight = 15
+            apLeftAnswerTextView?.lineHeight = 15 * BASE_SIZE_MULTIPLIER
+            apRightAnswerTextView?.lineHeight = 15 * BASE_SIZE_MULTIPLIER
         }
 
         apAnswersConstraintLayout?.run {
--- sdk/src/main/java/plus/adaptive/sdk/utils/DrawableUtils.kt
+++ sdk/src/main/java/plus/adaptive/sdk/utils/DrawableUtils.kt
@@ -6,13 +6,18 @@ import android.graphics.drawable.GradientDrawable
 
 internal fun createDrawableFromColor(
     color: Int?,
-    cornerRadius: Int? = null
+    cornerRadius: Int? = null,
+    strokeWidth: Int? = null,
+    strokeColor: Int? = null
 ): Drawable? {
     return color?.let {
         GradientDrawable().apply {
             setColor(it)
             cornerRadius?.let { radius ->
                 setCornerRadius(radius.toFloat())
+            }
+            if (strokeWidth != null && strokeColor != null) {
+                setStroke(strokeWidth, strokeColor)
             }
         }
     }
